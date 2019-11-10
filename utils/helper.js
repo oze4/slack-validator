5d3dc4bab86aea61da022cfe39871e428aa7fab3
Matt Oestreich <matthewpoestreich@gmail.com>
2019-11-10 17:29:07 -0600

    Update helper.js

--- utils/helper.js
+++ utils/helper.js
@@ -65,7 +65,7 @@ function validateRequestIsFromSlack(slackAppSigningSecret, slackVersionNumber, h
         }
 
         //let requestBody = httpReq.rawBody || httpReq.body.payload || httpReq.body;
-        let requestBody = httpReq.body
+        let requestBody = httpReq.get('x-raw-body');
         console.log(httpReq.headers);
         console.log(requestBody);
 
